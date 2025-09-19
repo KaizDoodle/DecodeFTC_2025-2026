3743e15829676e482180beba8576fee018a81cc4
KaizDoodle <13iak13@gmail.com>
2025-09-18 18:40:27 -0700

    merge all old commits into new repo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Testing/FieldCentricTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.OpModes.Testing;
+
+import org.firstinspires.ftc.teamcode.Config.Core.Util.Alliance;
+import org.firstinspires.ftc.teamcode.Config.Core.RobotContainer;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp
+public class FieldCentricTest extends OpMode {
+
+
+    RobotContainer robot;
+
+    public void configureOperator() {
+
+    }
+
+    @Override
+    public void init() {
+        robot = new RobotContainer(hardwareMap, gamepad1, gamepad2, Alliance.BLUE, telemetry);
+
+        robot.teleOpControl();
+    }
+
+    @Override
+    public void loop() {
+//        update();
+//        CommandScheduler.getInstance().reset();
+
+        robot.periodic();
+
+    }
+    @Override
+    public void start(){
+        robot.tStart();
+
+
+    }
+}
