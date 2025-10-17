49f55981ce028da46f03fdc0f34ae4de186b79f6
KaizDoodle <13iak13@gmail.com>
2025-10-16 18:45:12 -0700

    create sort color method half create intake subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Config/Subsystems/ColorSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Config/Subsystems/ColorSubsystem.java
@@ -1,4 +1,43 @@
 package org.firstinspires.ftc.teamcode.Config.Subsystems;
 
-public class ColorSubsystem {
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ColorSubsystem extends SubsystemBase {
+    ColorSensor sensorLeft;
+    ColorSensor sensorMiddle;
+    ColorSensor sensorRight;
+
+    String colorLeft;
+    String colorMiddle;
+    String colorRight;
+
+
+
+
+    public ColorSubsystem(HardwareMap hardwareMap) {
+        sensorLeft = hardwareMap.get(ColorSensor.class, "sensorLeft");
+        sensorMiddle = hardwareMap.get(ColorSensor.class, "sensorLeft");
+        sensorRight = hardwareMap.get(ColorSensor.class, "sensorLeft");
+    }
+    public String getColor(ColorSensor sensor) {
+        int red = sensor.red();
+        int green = sensor.green();
+        int blue = sensor.blue();
+        return "purple";
+    }
+
+//    return order left to right of colors
+    public String[] sortColor(){
+        String[] colors = new String[3];
+
+        colors[0] = getColor(sensorLeft);
+        colors[1] = getColor(sensorMiddle);
+        colors[2] = getColor(sensorRight);
+
+        return colors;
+    }
+
+
 }
